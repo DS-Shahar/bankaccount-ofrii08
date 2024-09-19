50d749d7ce841089c5e85b3e01efd2da59e1533a
ofrii08 <ofrikal2008@gmail.com>
2024-09-19 21:02:46 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+      BankAccount acct1 = new BankAccount(1000, -1000,"lisa");
+        BankAccount acct2 = new BankAccount (1000,-1000,"bon");
+        System.out.println(acct1);
+ System.out.println(acct2);
+        acct1.transfer(acct2,500);
+        System.out.println(acct1);
+        System.out.println(acct2);
+        acct1.transfer(acct2,1600);
+        System.out.println(acct1);
+        System.out.println(acct2);
+    }
 }
+
+
